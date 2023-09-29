8cc4d60d4caf6b01750bf7c9ec80f296ecd50666
blazerpaul15 <blazerpaul15@gmail.com>
2023-09-29 23:49:27 +0900

    vili: Build android.hardware.thermal@2.0 from source
    * Guard qti thermal hal as it doesn't work properly on vili.

--- device.mk
+++ device.mk
@@ -515,7 +515,13 @@ PRODUCT_COPY_FILES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor
+
+ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
+PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
+endif
 
 # Touch
 PRODUCT_PACKAGES += \
