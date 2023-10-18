f75f3845cba8eaba456cf0b64acf79a447898a04
althafvly <althafvly@gmail.com>
2023-10-18 17:33:43 +0900

    vili: Add IHighTouchPollingRate
    * re-add after cleanup for a14
    
    Change-Id: I75e4bc4386eb5ade0fce2c37b2579875670dc9e3
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -518,6 +518,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 endif
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.vili
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
